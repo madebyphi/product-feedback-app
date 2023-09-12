213f293013ce1a182a79304b4ecf70c48f2feef1
madebyphi <bawoni.phi@gmail.com>
2023-09-12 19:46:50 +0100

    Modify Text component to use content prop

--- src/components/utils/Text.tsx
+++ src/components/utils/Text.tsx
@@ -1,49 +1,50 @@
-import { PropsWithChildren } from "react";
+'use client'
 
 type As = "H1" | "H2" | "H3" | "H4" | "Body 1" | "Body 2" | "Body 3"
 
 interface TextProps {
     as: As,
-    children?: React.ReactNode
+    // children?: React.ReactNode
+    content: string
 }
 
 export default function Text(props: TextProps) {
-    const {children, as} = props
+    const {content, as} = props
     return(
         <>
             {as === "H1" &&
                 <h1 className="font-bold text-[1.5rem] tracking-[-0.0125em] leading-[2.1875rem]">
-                    {children}
+                    {content}
                 </h1>
             }
             {as === "H2" &&
                 <h2 className="font-bold text-[1.25rem] tracking-[-0.01375em] leading-[1.8125rem]">
-                    {children}
+                    {content}
                 </h2>
             }
             {as === "H3" &&
                 <h3 className="font-bold text-[1.125rem] tracking-[-0.01375em] leading-[1.625rem]">
-                    {children}
+                    {content}
                 </h3>
             }
             {as === "H4" &&
                 <h4 className="font-bold text-[0.875rem] tracking-[-0.01428em] leading-[1.25rem]">
-                    {children}
+                    {content}
                 </h4>
             }
             {as === "Body 1" &&
                 <p className="font-normal text-[1rem] leading-[1.4375rem]">
-                    {children}
+                    {content}
                 </p>
             }
             {as === "Body 2" &&
                 <p className="font-normal text-[0.9375rem] leading-[1.375rem]">
-                    {children}
+                    {content}
                 </p>
             }
             {as === "Body 3" &&
                 <p className="font-semibold text-[0.8125rem] leading-[1.1875rem]">
-                    {children}
+                    {content}
                 </p>
             }
         </>
