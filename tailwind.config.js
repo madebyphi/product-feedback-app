d9cbd9cd25638b08e87cf8e45d0122f17a2cdda7
madebyphi <bawoni.phi@gmail.com>
2023-09-15 20:57:45 +0100

    Install shadcn/ui

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        purple: "#AD1FEA",
+        blue: "#4661E6",
+        white: "#FFFFFF",
+        "gray-1": "#F7F8FD",
+        "gray-2": "#F2F4FF",
+        "gray-3": "#647196",
+        "dark-1": "#3A4374",
+        "dark-2": "#4661E6",
+        orange: "#F49F85",
+        cyan: "#62BCFA",
+        red: "#D73737",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
