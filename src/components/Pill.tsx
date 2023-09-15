86839d360a26afd908c2db1a59d0a14599b7bb00
madebyphi <bawoni.phi@gmail.com>
2023-09-15 20:58:29 +0100

    FIx bug

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,76 +1,96 @@
-'use client'
+"use client";
 
-import { ChevronUpIcon } from '@radix-ui/react-icons';
-import * as Toggle from '@radix-ui/react-toggle';
-import { useEffect, useState } from 'react';
+import { ChevronUpIcon } from "@radix-ui/react-icons";
+import * as Toggle from "@radix-ui/react-toggle";
+import { useEffect, useState } from "react";
 
 interface PillProps {
-    hasTopIcon?: boolean
-    content: number | string
+  hasTopIcon?: boolean;
+  content: number | string;
 }
 
 interface Colors {
-    bgColor: string
-    iconColor: string
-    contentColor: string
+  bgColor: string;
+  iconColor: string;
+  contentColor: string;
 }
 
-type State = 'default' | 'hover' | 'active'
+type State = "default" | "hover" | "active";
 
 export default function Pill(props: PillProps) {
-    const { content, hasTopIcon=false } = props
+  const { content, hasTopIcon = false } = props;
 
-    
-    const [active, setActive] = useState(false)
-    const [colors, setColors] = useState({bgColor: 'bg-gray-2', iconColor: 'text-blue', contentColor: 'text-dark-1'})
-    const [width, setWidth] = useState('')
-    
-    useEffect(() => {
-        if(hasTopIcon) {
-            setWidth('w-[2.5rem]')
-        }
-    }, [hasTopIcon])
+  const [active, setActive] = useState(false);
+  const [colors, setColors] = useState({
+    bgColor: "bg-gray-2",
+    iconColor: "text-blue",
+    contentColor: "text-dark-1",
+  });
+  const [width, setWidth] = useState("");
 
-    const updateColors = (state?: State) => {
-        switch (state) {
-            case 'hover':
-                setColors({ bgColor: 'bg-[#CFD7FF]', iconColor: 'text-blue', contentColor: 'text-dark-1' })
-                break;
-            case 'active':
-                setColors({ bgColor: 'bg-blue', iconColor: 'text-white', contentColor: 'text-white' })
-                break;
-            default: 
-                if (active) {
-                    return;
-                }
-                setColors({ bgColor: 'bg-[#F2F4FE]', iconColor: 'text-blue', contentColor: 'text-dark-1' })
-                return;
-        }
+  useEffect(() => {
+    if (hasTopIcon) {
+      setWidth("w-[2.5rem]");
     }
+  }, [hasTopIcon]);
 
-    const handleClick = () => {
-        if (!active) {
-            updateColors('active')
-            setActive(true)
-        } else {
-            updateColors('hover')
-            setActive(false)
+  const updateColors = (state?: State) => {
+    switch (state) {
+      case "hover":
+        if (active) {
+          return;
         }
+        setColors({
+          bgColor: "bg-[#CFD7FF]",
+          iconColor: "text-blue",
+          contentColor: "text-dark-1",
+        });
+        break;
+      case "active":
+        setColors({
+          bgColor: "bg-blue",
+          iconColor: "text-white",
+          contentColor: "text-white",
+        });
+        break;
+      default:
+        if (active) {
+          return;
+        }
+        setColors({
+          bgColor: "bg-[#F2F4FE]",
+          iconColor: "text-blue",
+          contentColor: "text-dark-1",
+        });
+        return;
     }
+  };
 
-    return(
-        <Toggle.Root 
-            className={`flex flex-col items-center ${width} ${colors.bgColor} px-[1rem] py-[0.375rem] text-dark-1 rounded-[0.625rem]`}
-            onMouseEnter={() => updateColors('hover')}
-            onMouseLeave={() => updateColors()}
-            onClick={handleClick}
-        >
-            {
-                hasTopIcon && <ChevronUpIcon fontWeight={700} className={`${colors.iconColor}`}/>
-            }
-            <p className={`${colors.contentColor} font-bold text-[13px] leading-[19px] tracking-[-0.18px]`}>
-                {content}
-            </p>
-        </Toggle.Root>
-    )
+  const handleClick = () => {
+    if (!active) {
+      updateColors("active");
+      setActive(true);
+    } else {
+      updateColors("hover");
+      setActive(false);
+    }
+  };
+
+  return (
+    <Toggle.Root
+      className={`flex flex-col items-center ${width} ${colors.bgColor} px-[1rem] py-[0.375rem] text-dark-1 rounded-[0.625rem]`}
+      onMouseEnter={() => updateColors("hover")}
+      onMouseLeave={() => updateColors()}
+      onClick={handleClick}
+    >
+      {hasTopIcon && (
+        <ChevronUpIcon fontWeight={700} className={`${colors.iconColor}`} />
+      )}
+      <p
+        className={`${colors.contentColor} font-bold text-[13px] leading-[19px] tracking-[-0.18px]`}
+      >
+        {content}
+      </p>
+    </Toggle.Root>
+  );
 }
