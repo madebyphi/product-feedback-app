0b162d062b6ca22e823bcf0b1e8b8426011ad675
madebyphi <bawoni.phi@gmail.com>
2023-09-28 09:49:42 +0100

    Build FeedbackBar component

---
+++ src/components/composed/FeedbackBar.tsx
@@ -0,0 +1,62 @@
+import { Fragment } from "react";
+import Button from "@/components/Button";
+import { Dropdown } from "@/components/Dropdown";
+import Text from "@/components/Text";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  // SelectLabel,
+  SelectTrigger,
+  SelectValue,
+  SelectSeparator,
+} from "@/components/ui/select";
+
+import { AwardIcon } from "lucide-react";
+
+const options = [
+  "Most Upvotes",
+  "Least Upvotes",
+  "Most Comments",
+  "Least Comments",
+];
+
+export default function FeedbackBar() {
+  return (
+    <div className="flex items-center bg-dark-1 w-full rounded-[0.625rem] pl-[2rem] py-[0.875rem] pr-[1rem] justify-between">
+      <div className="flex gap-[2.375rem] items-center">
+        <div className="flex gap-[1rem]">
+          <AwardIcon />
+          <Text as="H3" content={`6 Suggestions`} />
+        </div>
+        {/* <Dropdown items={options} placeholder="change this" /> */}
+        <div className="flex items-center gap-[0.3rem]">
+          <p className="font-normal text-[0.875rem] leading-[1.25rem]">
+            Sort by :
+          </p>
+          <Select defaultValue={"Most Upvotes"}>
+            <SelectTrigger className="w-[7rem]  font-bold rounded-[0.3125rem] bg-transparent border-0 px-0 text-[0.875rem] leading-[1.25rem]">
+              <SelectValue placeholder={"Most Upvotes"} />
+            </SelectTrigger>
+            <SelectContent className="text-[1rem] leading-[1.4375rem] mt-[1.875rem]">
+              <SelectGroup>
+                {/* <SelectLabel>Fruits</SelectLabel> */}
+                {options.map((item: string, index: number, arr) => {
+                  return (
+                    <Fragment key={item}>
+                      <SelectItem value={item}>{item}</SelectItem>
+                      {arr.length > index + 1 && <SelectSeparator />}
+                    </Fragment>
+                  );
+                })}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <Button text="+ Add Feedback" />
+    </div>
+  );
+}
