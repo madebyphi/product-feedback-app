b35951da4e44a39e33ec3b06e998b82410766169
madebyphi <bawoni.phi@gmail.com>
2023-10-06 08:58:39 +0100

    centralize types

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+import { Category } from "./components/composed/CategoryCard";
+
+export type Status = "suggestion" | "planned" | "in-progress" | "live";
+
+export interface User {
+  name: string;
+  username: string;
+  image: string;
+}
+
+export interface Comment {
+  id: number;
+  content: string;
+  user: User
+}
+
+export interface ProductRequest {
+  id: number;
+  title: string;
+  description: string;
+  category: Category;
+  status: Status;
+  upvotes: number;
+  comments: Comment[],
+};
