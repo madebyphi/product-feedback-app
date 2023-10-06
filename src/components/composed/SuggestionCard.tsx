524f641aa9b8ef7648d3d2115605047787f71341
madebyphi <bawoni.phi@gmail.com>
2023-10-06 09:05:29 +0100

    change icons to project svg

--- src/components/composed/SuggestionCard.tsx
+++ src/components/composed/SuggestionCard.tsx
@@ -2,25 +2,20 @@ import { Toggle } from "@/components/ui/toggle";
 import { ChevronUp, MessageCircle } from "lucide-react";
 import { useState } from "react";
 import Text from "@/components/Text";
-// {
-//   id: 0,
-//   count: 112,
-//   title: "Add tags for solutions",
-//   description: "Easier to search for solutions based on a specific task",
-//   category: "Enhancement",
-//   comments: 2,
-// },
+import { ProductRequest } from "@/types";
+import { Svg } from "../Svg";
 
-export default function SuggestionCard(props: any) {
+export default function SuggestionCard(props: { suggestion: ProductRequest }) {
   const { suggestion } = props;
   const [count, setCount] = useState(0);
   return (
     <div className="flex bg-white px-[2rem] py-[1.75rem] rounded-[0.625rem] justify-between">
       <div className="flex gap-[2.5rem]">
-        <Toggle className="flex flex-col gap-[0.5rem] h-[53px] bg-gray-2 first:text-blue [&>p]:text-dark-1 hover:bg-[#CFD7FF] hover:text-blue data-[state=on]:bg-blue data-[state=on]:text-white  [&>p]:data-[state=on]:text-white font-semibold rounded-[0.625rem] px-[0.6875rem] pb-[0.5rem] pt-[0.875rem]">
-          <ChevronUp />
+        <Toggle className="flex flex-col h-auto self-start gap-[0.5rem] bg-gray-2 [&>p]:text-dark-1 [&>svg]:stroke-blue hover:bg-[#CFD7FF] data-[state=on]:bg-blue [&>p]:data-[state=on]:text-white [&>svg]:data-[state=on]:stroke-white font-semibold rounded-[0.625rem] px-[0.6875rem] pb-[0.5rem] pt-[0.875rem]">
+          {/* <ChevronUp /> */}
+          <Svg name="up-arrow" className="shrink-0" />
           <p className="font-bold text-[0.8125rem] leading-[1.1875rem] tracking-[-0.01125rem]">
-            {count | suggestion.count}
+            {count | suggestion.upvotes}
           </p>
         </Toggle>
         <div className="flex flex-col gap-[0.75rem] ">
@@ -32,15 +27,15 @@ export default function SuggestionCard(props: any) {
               {suggestion.description}
             </p>
           </div>
-          <Toggle className="bg-gray-2 text-blue hover:bg-[#CFD7FF] hover:text-blue data-[state=on]:bg-blue data-[state=on]:text-white font-semibold rounded-[0.625rem] px-[1rem] py-[0.375rem] self-start">
+          <Toggle className="bg-gray-2 text-blue hover:bg-[#CFD7FF] hover:text-blue data-[state=on]:bg-blue data-[state=on]:text-white font-semibold rounded-[0.625rem] px-[1rem] py-[0.375rem] self-start capitalize">
             <Text as="Body 3" content={suggestion.category} />
           </Toggle>
         </div>
       </div>
-      <div className="flex gap-[0.5rem] text-dark-1 self-center">
-        <MessageCircle className="text-[#CDD2EE]" />
+      <div className="flex gap-[0.5rem] text-dark-1 self-center items-center">
+        <Svg name="comments" />
         <p className="font-bold text-[1rem] leading-[1.4375rem] tracking-[0.01375rem]">
-          {suggestion.comments}
+          {suggestion.comments?.length | 0}
         </p>
       </div>
     </div>
