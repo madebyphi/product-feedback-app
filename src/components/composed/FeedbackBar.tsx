18ba6c15ed982527973b78c1884b478dda5b43d9
madebyphi <bawoni.phi@gmail.com>
2023-10-06 09:04:41 +0100

    change Icons to project svg + accessibility change

--- src/components/composed/FeedbackBar.tsx
+++ src/components/composed/FeedbackBar.tsx
@@ -14,7 +14,7 @@ import {
   SelectSeparator,
 } from "@/components/ui/select";
 
-import { AwardIcon } from "lucide-react";
+import { Svg } from "@/components/Svg";
 
 const options = [
   "Most Upvotes",
@@ -25,19 +25,26 @@ const options = [
 
 export default function FeedbackBar() {
   return (
-    <div className="flex items-center bg-dark-1 w-full rounded-[0.625rem] pl-[2rem] py-[0.875rem] pr-[1rem] justify-between">
+    <div className="flex items-center bg-dark-1 w-full md:rounded-[0.625rem] pl-[2rem] py-[0.875rem] pr-[1rem] justify-between">
       <div className="flex gap-[2.375rem] items-center">
         <div className="flex gap-[1rem]">
-          <AwardIcon />
+          {/* <AwardIcon /> */}
+          <Svg name="suggestions" />
           <Text as="H3" content={`6 Suggestions`} />
         </div>
         {/* <Dropdown items={options} placeholder="change this" /> */}
         <div className="flex items-center gap-[0.3rem]">
-          <p className="font-normal text-[0.875rem] leading-[1.25rem]">
+          <p
+            id="sort-label"
+            className="font-normal text-[0.875rem] leading-[1.25rem]"
+          >
             Sort by :
           </p>
           <Select defaultValue={"Most Upvotes"}>
-            <SelectTrigger className="w-[7rem]  font-bold rounded-[0.3125rem] bg-transparent border-0 px-0 text-[0.875rem] leading-[1.25rem]">
+            <SelectTrigger
+              aria-labelledby="sort-label"
+              className="w-[7rem]  font-bold rounded-[0.3125rem] bg-transparent border-0 px-0 text-[0.875rem] leading-[1.25rem]"
+            >
               <SelectValue placeholder={"Most Upvotes"} />
             </SelectTrigger>
             <SelectContent className="text-[1rem] leading-[1.4375rem] mt-[1.875rem]">
