8f17e2f094f992892e876955da357ec2c635b7e0
madebyphi <bawoni.phi@gmail.com>
2023-10-06 09:03:27 +0100

    Edit Text component to accept styles

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -15,47 +15,47 @@ export default function Text(props: TextProps) {
     <>
       {as === "H1" && (
         <h1
-          className={`font-bold text-[1.5rem] tracking-[-0.0125em] leading-[2.1875rem] ${classes}`}
+          className={`font-bold text-[1.5rem] tracking-[-0.0125em] leading-[2.1875rem]`}
         >
           {content}
         </h1>
       )}
       {as === "H2" && (
         <h2
-          className={`font-bold text-[1.25rem] tracking-[-0.01375em] leading-[1.8125rem] ${classes}`}
+          className={`${classes} font-bold text-[1.25rem] tracking-[-0.01375em] leading-[1.8125rem]`}
         >
           {content}
         </h2>
       )}
       {as === "H3" && (
         <h3
-          className={`font-bold text-[1.125rem] tracking-[-0.01375em] leading-[1.625rem] ${classes}`}
+          className={`${classes} font-bold text-[1.125rem] tracking-[-0.01375em] leading-[1.625rem]`}
         >
           {content}
         </h3>
       )}
       {as === "H4" && (
         <h4
-          className={`font-bold text-[0.875rem] tracking-[-0.01428em] leading-[1.25rem] ${classes}`}
+          className={`${classes} font-bold text-[0.875rem] tracking-[-0.01428em] leading-[1.25rem]`}
         >
           {content}
         </h4>
       )}
       {as === "Body 1" && (
-        <p className={`font-normal text-[1rem] leading-[1.4375rem] ${classes}`}>
+        <p className={`${classes} font-normal text-[1rem] leading-[1.4375rem]`}>
           {content}
         </p>
       )}
       {as === "Body 2" && (
         <p
-          className={`font-normal text-[0.9375rem] leading-[1.375rem] ${classes}`}
+          className={`${classes} font-normal text-[0.9375rem] leading-[1.375rem]`}
         >
           {content}
         </p>
       )}
       {as === "Body 3" && (
         <p
-          className={`font-semibold text-[0.8125rem] leading-[1.1875rem] ${classes}`}
+          className={`${classes} font-semibold text-[0.8125rem] leading-[1.1875rem]`}
         >
           {content}
         </p>
