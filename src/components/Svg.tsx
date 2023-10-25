21ed0833b51a1e426b7fc811f3a37c95f0ebd54c
madebyphi <bawoni.phi@gmail.com>
2023-10-25 08:53:28 +0100

    Add react-preferred names for inline properties

--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -30,10 +30,9 @@ export function Svg(props: SvgProps) {
         >
           <path
             d="M6 9L2 5l4-4"
-            stroke="#4661E6"
-            stroke-width="2"
+            strokeWidth="2"
             fill="none"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
         </svg>
       );
@@ -47,10 +46,9 @@ export function Svg(props: SvgProps) {
         >
           <path
             d="M1 1l4 4 4-4"
-            stroke="#4661E6"
-            stroke-width="2"
+            strokeWidth="2"
             fill="none"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
         </svg>
       );
@@ -65,9 +63,9 @@ export function Svg(props: SvgProps) {
           <path
             d="M1 6l4-4 4 4"
             // stroke="#4661E6"
-            stroke-width="2"
+            strokeWidth="2"
             fill="none"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
         </svg>
       );
@@ -82,7 +80,7 @@ export function Svg(props: SvgProps) {
           <path
             fill="none"
             stroke="#AD1FEA"
-            stroke-width="2"
+            strokeWidth="2"
             d="M1 5.233L4.522 9 12 1"
           />
         </svg>
@@ -98,7 +96,7 @@ export function Svg(props: SvgProps) {
           <path
             d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z"
             fill="#FFF"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
         </svg>
       );
@@ -113,7 +111,7 @@ export function Svg(props: SvgProps) {
           <path
             d="M2.62 16H1.346l.902-.91c.486-.491.79-1.13.872-1.823C1.036 11.887 0 9.89 0 7.794 0 3.928 3.52 0 9.03 0 14.87 0 18 3.615 18 7.455c0 3.866-3.164 7.478-8.97 7.478-1.017 0-2.078-.137-3.025-.388A4.705 4.705 0 012.62 16z"
             fill="#CDD2EE"
-            fill-rule="nonzero"
+            fillRule="nonzero"
           />
         </svg>
       );
@@ -134,17 +132,17 @@ export function Svg(props: SvgProps) {
               r="166.816%"
               id="a"
             >
-              <stop stop-color="#E84D70" offset="0%" />
-              <stop stop-color="#A337F6" offset="53.089%" />
-              <stop stop-color="#28A7ED" offset="100%" />
+              <stop stopColor="#E84D70" offset="0%" />
+              <stop stopColor="#A337F6" offset="53.089%" />
+              <stop stopColor="#28A7ED" offset="100%" />
             </radialGradient>
           </defs>
-          <g fill="none" fill-rule="evenodd">
+          <g fill="none" fillRule="evenodd">
             <circle fill="url(#a)" cx="20" cy="20" r="20" />
             <path
               d="M19.512 15.367l4.975 4.53-3.8 5.54L11.226 29l4.485-4.1c.759.275 1.831.026 2.411-.594a1.958 1.958 0 00-.129-2.82c-.836-.745-2.199-.745-2.964.068-.57.607-.767 1.676-.44 2.381L11 28.713c.255-1.06.683-2.75 1.115-4.436l.137-.531c.658-2.563 1.287-4.964 1.287-4.964l5.973-3.415zM23.257 12L28 16.443l-2.584 2.606-4.89-4.583L23.257 12z"
               fill="#FFF"
-              fill-rule="nonzero"
+              fillRule="nonzero"
             />
           </g>
         </svg>
@@ -157,7 +155,7 @@ export function Svg(props: SvgProps) {
           height="17"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g fill="#FFF" fill-rule="evenodd">
+          <g fill="#FFF" fillRule="evenodd">
             <path d="M0 0h20v3H0zM0 7h20v3H0zM0 14h20v3H0z" />
           </g>
         </svg>
@@ -179,16 +177,16 @@ export function Svg(props: SvgProps) {
               r="166.816%"
               id="a"
             >
-              <stop stop-color="#E84D70" offset="0%" />
-              <stop stop-color="#A337F6" offset="53.089%" />
-              <stop stop-color="#28A7ED" offset="100%" />
+              <stop stopColor="#E84D70" offset="0%" />
+              <stop stopColor="#A337F6" offset="53.089%" />
+              <stop stopColor="#28A7ED" offset="100%" />
             </radialGradient>
           </defs>
-          <g fill="none" fill-rule="evenodd">
+          <g fill="none" fillRule="evenodd">
             <circle fill="url(#a)" cx="28" cy="28" r="28" />
             <path
               fill="#FFF"
-              fill-rule="nonzero"
+              fillRule="nonzero"
               d="M30.343 36v-5.834h5.686v-4.302h-5.686V20h-4.597v5.864H20v4.302h5.746V36z"
             />
           </g>
@@ -205,7 +203,7 @@ export function Svg(props: SvgProps) {
           <text
             transform="translate(-24 -20)"
             fill="#F2F4FE"
-            fill-rule="evenodd"
+            fillRule="evenodd"
             font-family="Jost-Bold, Jost"
             font-size="14"
             font-weight="bold"
@@ -227,7 +225,7 @@ export function Svg(props: SvgProps) {
           <path
             d="M11.5 2.274c2.237 0 4.339.854 5.923 2.408a8.123 8.123 0 012.465 5.839 8.084 8.084 0 01-1.7 4.979 8.457 8.457 0 01-3.652 2.71l-.31.112.003.826h.369c.262 0 .475.21.475.469a.47.47 0 01-.39.46l-.085.008h-.365l.004 1.02h.36c.263 0 .476.21.476.469a.47.47 0 01-.39.461l-.085.008h-.358l.006 1.487a.466.466 0 01-.381.46l-.094.01H9.23a.478.478 0 01-.466-.378l-.01-.092.006-1.487h-.357a.472.472 0 01-.475-.47.47.47 0 01.39-.46l.085-.008h.361l.004-1.02h-.365a.472.472 0 01-.475-.468.47.47 0 01.39-.462l.085-.007h.368l.004-.826a8.452 8.452 0 01-3.996-2.867 8.08 8.08 0 01-1.666-5.056c.032-2.127.923-4.152 2.511-5.7 1.508-1.471 3.448-2.322 5.493-2.416l.324-.009h.06zm1.791 19.769H9.709l-.004 1.02h3.59l-.004-1.02zm-.007-1.958H9.716l-.003 1.02h3.574l-.003-1.02zM11.5 3.212h-.054c-3.946.027-7.327 3.325-7.384 7.2-.048 3.266 2.14 6.192 5.322 7.118.174.05.3.193.332.364l.008.088-.004 1.166h3.56l-.004-1.166a.47.47 0 01.34-.452c3.134-.912 5.323-3.794 5.323-7.01a7.197 7.197 0 00-2.185-5.173A7.453 7.453 0 0011.5 3.212zm.829 1.782a.4.4 0 01.401.397v.322c.48.12.932.307 1.346.552l.228-.226a.405.405 0 01.569 0L16.046 7.2a.393.393 0 010 .56l-.23.228c.247.41.437.858.557 1.333h.323a.4.4 0 01.402.397v1.645a.4.4 0 01-.402.396h-.323c-.12.476-.31.924-.557 1.333l.23.228a.393.393 0 010 .56l-1.173 1.163a.405.405 0 01-.57 0l-.227-.227a5.02 5.02 0 01-1.346.553v.322a.4.4 0 01-.401.396H10.67a.4.4 0 01-.402-.396v-.322a5.022 5.022 0 01-1.345-.553l-.228.227a.405.405 0 01-.569 0L6.954 13.88a.393.393 0 010-.56l.23-.228a4.924 4.924 0 01-.557-1.333h-.324a.4.4 0 01-.401-.396V9.719a.4.4 0 01.401-.397h.324c.12-.475.31-.923.557-1.333l-.23-.228a.393.393 0 010-.56L8.127 6.04a.405.405 0 01.569 0l.228.226a5.021 5.021 0 011.345-.552V5.39a.4.4 0 01.402-.397zM11.5 7.721c-1.572 0-2.846 1.263-2.846 2.82 0 1.558 1.274 2.82 2.846 2.82s2.846-1.262 2.846-2.82c0-1.557-1.274-2.82-2.846-2.82zm11.025 4.152c.262 0 .475.21.475.469a.47.47 0 01-.39.461l-.085.008h-.498a.472.472 0 01-.475-.469.47.47 0 01.39-.461l.085-.008h.498zm-21.552 0c.262 0 .475.21.475.469a.47.47 0 01-.39.461l-.085.008H.475A.472.472 0 010 12.342a.47.47 0 01.39-.461l.085-.008h.498zM3.112 3.45l.074.06.46.451c.185.183.186.48 0 .663a.476.476 0 01-.596.062l-.075-.06-.459-.451a.465.465 0 01-.001-.663.48.48 0 01.597-.062zm17.373.062c.162.16.182.408.06.59l-.061.073-.46.45a.476.476 0 01-.67 0 .464.464 0 01-.06-.59l.06-.074.46-.45a.48.48 0 01.671 0zM11.5 0c.233 0 .427.166.467.384l.008.085v.49a.472.472 0 01-.475.468.473.473 0 01-.467-.384l-.008-.084v-.49c0-.26.213-.469.475-.469z"
             fill="#FFF"
-            fill-rule="nonzero"
+            fillRule="nonzero"
           />
         </svg>
       );
@@ -239,13 +237,13 @@ export function Svg(props: SvgProps) {
           height="108"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g fill-rule="nonzero" fill="none" opacity=".5">
+          <g fillRule="nonzero" fill="none" opacity=".5">
             <path
               d="M48.73 15.593C25.822 15.59 7.246 34.224 7.235 57.22c-.01 22.997 18.55 41.649 41.458 41.665 22.909.016 41.494-18.61 41.516-41.606a41.72 41.72 0 00-12.132-29.473A41.4 41.4 0 0048.73 15.593z"
               stroke="#3A4374"
-              stroke-width="1.045"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.045"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <ellipse
               fill="#231F20"
@@ -348,9 +346,9 @@ export function Svg(props: SvgProps) {
           <path
             d="M1 1l4 4 4-4"
             stroke="#4661E6"
-            stroke-width="2"
+            strokeWidth="2"
             fill="none"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
         </svg>
       );
