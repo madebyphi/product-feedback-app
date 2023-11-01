9ad3c1d9b8d70d45437955e183d5a3917da96172
madebyphi <bawoni.phi@gmail.com>
2023-11-01 16:14:28 +0100

    Add features to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,29 +3,36 @@
 import { ChevronLeftIcon } from "@radix-ui/react-icons";
 import { Button as Btn } from "@/components/ui/button";
 import { LucideIcon, ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import React from "react";
 
 type ButtonColor = "purple" | "blue" | "dark-1" | "red";
 type As = "link" | "button";
 
 type IconType = LucideIcon;
-interface CommonButtonProps {
+type CustomButtonProps = {
   color?: ButtonColor;
   text: string;
   as?: As;
   className?: string;
   onClick?: () => void;
-}
-
-type HasIconProps = CommonButtonProps & {
-  hasLeftIcon: true;
-  Icon: IconType;
 };
-type NoIconProps = CommonButtonProps & { hasLeftIcon?: false };
+
+type CommonButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+type HasIconProps = CommonButtonProps &
+  CustomButtonProps & {
+    hasLeftIcon: true;
+    Icon: IconType;
+  };
+type NoIconProps = CommonButtonProps &
+  CustomButtonProps & { hasLeftIcon?: false };
 
 export default function Button(props: NoIconProps): JSX.Element;
 export default function Button(props: HasIconProps): JSX.Element;
 export default function Button(
-  props: CommonButtonProps & { hasLeftIcon?: boolean; Icon?: IconType }
+  props: CommonButtonProps &
+    CustomButtonProps & { hasLeftIcon?: boolean; Icon?: IconType }
 ) {
   let {
     color = "purple",
@@ -35,6 +42,7 @@ export default function Button(
     Icon = ChevronDown,
     className,
     onClick,
+    ...rest
   } = props;
 
   let colors = {
@@ -56,6 +64,7 @@ export default function Button(
       <Btn
         onClick={onClick}
         className={`flex items-center justify-center gap-[0.9rem] p-0 cursor-pointer bg-transparent active:bg-transparent hover:bg-transparent focus:bg-transparent`}
+        {...rest}
       >
         {hasLeftIcon && <ChevronLeftIcon color="#4661E6" />}
         <span
@@ -70,7 +79,11 @@ export default function Button(
   return (
     <Btn
       onClick={onClick}
-      className={`${colors[color]} ${hoverColors[color]} color-[#FFFFFF] font-bold text-[0.875rem] leading-[1.25rem] rounded-[0.625rem] px-[1rem] py-[10.5px] md:px-[1.5rem] md:py-[12px] capitalize cursor-pointer`}
+      className={cn(
+        `${colors[color]} ${hoverColors[color]} color-[#FFFFFF] font-bold text-[0.875rem] leading-[1.25rem] rounded-[0.625rem] px-[1rem] py-[10.5px] md:px-[1.5rem] md:py-[12px] capitalize cursor-pointer`,
+        className
+      )}
+      {...rest}
     >
       {hasLeftIcon && <Icon />}
       {text}
